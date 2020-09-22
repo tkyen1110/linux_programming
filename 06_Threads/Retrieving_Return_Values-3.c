d8d8c51cf47297cd5cb4d39dc79ed1c408b293e0
tkyen <tkyen1110@gmail.com>
2020-09-22 23:13:20 +0800

    Copy from cadtc course

---
+++ 06_Threads/Retrieving_Return_Values-3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <pthread.h>
+double ret=0.0;
+void *thread_func(void *arg) 
+{
+	//non portable code
+	fprintf(stderr, "tid:%x\n", (int)pthread_self());
+	ret=1.0f;
+	pthread_detach(pthread_self());
+	fprintf(stderr, "in thread_func : %.1f\n", ret);
+	return NULL;
+}
+int main(void)
+{
+	int error;
+	pthread_t tid;
+	if((error=pthread_create(&tid, NULL, thread_func, NULL)))
+		fprintf(stderr, "Failed to create thread: %d\n", strerror(error));
+	if((error=pthread_join(tid, NULL)))
+		fprintf(stderr, "Failed to join thread: %d\n", strerror(error));
+	fprintf(stderr, "in main : %.1f\n", ret);
+	return 0;
+}
+
